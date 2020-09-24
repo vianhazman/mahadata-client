c71271c52a5f7b12e0f4750fd0237f64e0c78264
Favian Hazman <vianhazman@Favians-MacBook-Pro.local>
2020-09-24 22:37:53 +0700

    global event, unselect tooltip, remove case from city toggle

--- src/services/HeatMap/annotation.js
+++ src/services/HeatMap/annotation.js
@@ -1,11 +1,5 @@
 const Annotation = {
-  "Jakarta Selatan": [
-    {
-      startTime: "2020-04-10",
-      endTime: "2020-06-10",
-      title: "PSBB Tahap 1",
-      type: "RANGE",
-    },
+  Global: [
     {
       startTime: "2020-05-24",
       endTime: "2020-05-25",
@@ -17,6 +11,76 @@ const Annotation = {
       endTime: "2020-07-31",
       title: "Idul Adha",
       type: "EVENT",
+    },
+  ],
+  "Jakarta Selatan": [
+    {
+      startTime: "2020-04-10",
+      endTime: "2020-06-10",
+      title: "PSBB Tahap 1",
+      type: "RANGE",
+    },
+    {
+      startTime: "2020-09-14",
+      endTime: "2020-09-28",
+      title: "PSBB Tahap 2",
+      type: "RANGE",
+    },
+  ],
+  "Jakarta Barat": [
+    {
+      startTime: "2020-04-10",
+      endTime: "2020-06-10",
+      title: "PSBB Tahap 1",
+      type: "RANGE",
+    },
+    {
+      startTime: "2020-09-14",
+      endTime: "2020-09-28",
+      title: "PSBB Tahap 2",
+      type: "RANGE",
+    },
+  ],
+  "Jakarta Timur": [
+    {
+      startTime: "2020-04-10",
+      endTime: "2020-06-10",
+      title: "PSBB Tahap 1",
+      type: "RANGE",
+    },
+    {
+      startTime: "2020-09-14",
+      endTime: "2020-09-28",
+      title: "PSBB Tahap 2",
+      type: "RANGE",
+    },
+  ],
+  "Jakarta Pusat": [
+    {
+      startTime: "2020-04-10",
+      endTime: "2020-06-10",
+      title: "PSBB Tahap 1",
+      type: "RANGE",
+    },
+    {
+      startTime: "2020-09-14",
+      endTime: "2020-09-28",
+      title: "PSBB Tahap 2",
+      type: "RANGE",
+    },
+  ],
+  "Jakarta Utara": [
+    {
+      startTime: "2020-04-10",
+      endTime: "2020-06-10",
+      title: "PSBB Tahap 1",
+      type: "RANGE",
+    },
+    {
+      startTime: "2020-09-14",
+      endTime: "2020-09-28",
+      title: "PSBB Tahap 2",
+      type: "RANGE",
     },
   ],
 };
