11750ef71571966eae37c7dac7d8dc89f0c7cbc4
Muhamad Istiady Kartadibrata <mkartadibrata@gmail.com>
2020-09-25 11:36:58 +0700

    collapsable legend

--- src/components/LegendContainer/styled.js
+++ src/components/LegendContainer/styled.js
@@ -7,12 +7,19 @@ export const StyledWrapper = styled(Paper)`
   right: 0;
   padding: 1% 1% 1% 1%;
   margin: 2% 2% 2% 2%;
-  width: 15vw;
+  width: 200px;
   background-color: rgba(255, 255, 255, 0.75);
-  h3 {
-    margin-top: 0;
-    margin-bottm: 1rem;
-  }
+  ${({ isOpen }) =>
+    isOpen &&
+    `
+animation: show 0.3s linear;
+animation-fill-mode: forwards;
+@keyframes show {
+  0%   {height: 25px;}
+ 
+  100% {height: 320px;}
+}`}
+  ${({ isOpen }) => !isOpen && "height:25px;"};
 `;
 
 export const WrapperBox = styled(Box)`
@@ -31,4 +38,34 @@ export const Color = styled(Box)`
   background-color: ${({ color }) => color};
   height: 100%;
   width: 10%;
+`;
+export const WrapperTitle = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h3 {
+    margin: 0;
+  }
+  margin-bottom: 1rem;
+  svg {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
+export const WrapperLegend = styled.div`
+  ${({ isOpen }) =>
+    isOpen &&
+    `
+  animation: block 1s;
+  animation-fill-mode: forwards;
+  @keyframes block {
+    0%   {opacity: 0;}
+   
+    100% {opacity: 1;}
+  }
+
+
+  `}
+  ${({ isOpen }) => !isOpen && "opacity:0"};
 `;
