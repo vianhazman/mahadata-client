67c39a19939060976bbb3493db44c59349cc97c8
Ray Azrin Karim <rayazrin19@gmail.com>
2020-09-30 12:46:53 +0700

    clickable and playable province map

--- src/components/LayeredMap/utils.js
+++ src/components/LayeredMap/utils.js
@@ -14,6 +14,12 @@ export const randomRgba = (tile, toggleData, heatData) => {
         ? JSON.parse(dataDate[tile.properties.kab].change_color)
         : JSON.parse(dataDate[tile.properties.kab].ratio_color);
     return i;
+  } else if (dataDate[tile.properties.Propinsi]) {
+    let i =
+      toggleData === "Mobilitas"
+        ? JSON.parse(dataDate[tile.properties.Propinsi].change_color)
+        : JSON.parse(dataDate[tile.properties.Propinsi].ratio_color);
+    return i;
   }
   return [255, 255, 255];
 };
