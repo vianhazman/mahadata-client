8be41364150254ecf1d66d07c4c9846889cc35b4
Ray Azrin Karim <rayazrin19@gmail.com>
2020-10-05 15:04:17 +0700

    fix bug when hovering map using touch devices

--- src/components/HoverTooltip/utils.js
+++ src/components/HoverTooltip/utils.js
@@ -5,10 +5,18 @@ import { Box, Typography } from "@material-ui/core";
 import { TOGGLE } from "../../constants/MapConstants";
 export const getChangeCaption = (hoverInfo, heatData, toggle, toggleData) => {
   const toggleType = toggleData === "Mobilitas" ? "change" : "ratio";
-  const data =
-    toggle === TOGGLE.CITY
-      ? heatData.data[hoverInfo.object.properties.kab][toggleType]
-      : heatData.data[hoverInfo.object.properties.Propinsi][toggleType];
+  const isDistrict = toggle === TOGGLE.CITY;
+  const isProvince = toggle === TOGGLE.PROVINCE;
+  const isMobility = toggleData === "Mobilitas";
+  const isRatio = toggleData === "Ratio";
+  const district = heatData.data[hoverInfo.object.properties.kab];
+  const province = heatData.data[hoverInfo.object.properties.Propinsi];
+  if (
+    (isDistrict && typeof district === "undefined") ||
+    (isProvince && typeof province === "undefined")
+  )
+    return "";
+  const data = isDistrict ? district[toggleType] : province[toggleType];
   if (data <= 0) {
     return (
       <Box
@@ -17,7 +25,7 @@ export const getChangeCaption = (hoverInfo, heatData, toggle, toggleData) => {
         justifyContent="center"
         fontWeight="bold"
       >
-        {toggleData === "Mobilitas" && <TrendingDownIcon />}
+        {isMobility && <TrendingDownIcon />}
 
         <Typography variant="h4" fontStyle="oblique">
           <Box fontWeight="bold" ml={1}>
@@ -29,7 +37,7 @@ export const getChangeCaption = (hoverInfo, heatData, toggle, toggleData) => {
   } else {
     return (
       <Box display="flex" alignItems="center" justifyContent="center">
-        {toggleData === "Ratio" && <TrendingUpIcon />}
+        {isRatio && <TrendingUpIcon />}
         <Typography variant="h4" fontStyle="oblique">
           <Box fontWeight="bold" ml={1}>
             {data}%
